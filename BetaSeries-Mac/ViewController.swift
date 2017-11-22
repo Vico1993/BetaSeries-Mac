8151c4f16ff302dc85d2f17354424a8bd3f65402
Victor Piolin <victor.piolin@gmail.com>
2017-11-22 17:35:28 +0100

    Initial Commit

---
+++ BetaSeries-Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BetaSeries-Mac
+//
+//  Created by Victor Piolin on 22/11/2017.
+//  Copyright © 2017 Victor Piolin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
