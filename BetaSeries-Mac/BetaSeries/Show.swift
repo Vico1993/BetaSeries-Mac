878ebf86cde9197cebd074e007e46c817fa915c9
Victor Piolin <victor.piolin@gmail.com>
2017-11-23 13:16:27 +0100

    // -> Two new class to handle Betaseries Return

---
+++ BetaSeries-Mac/BetaSeries/Show.swift
@@ -0,0 +1,13 @@
+//
+//  Show.swift
+//  BetaSeries-Mac
+//
+//  Created by Victor Piolin on 23/11/2017.
+//  Copyright © 2017 Victor Piolin. All rights reserved.
+//
+
+import Foundation
+
+class show {
+    
+}
