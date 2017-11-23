f8d8251e97d75b8029cfaaec588c7b4b796330ba
Victor Piolin <victor.piolin@gmail.com>
2017-11-23 14:20:33 +0100

    // Creation of big dictionary to contains all betaseries data. // Each Show contain his episode.

--- BetaSeries-Mac/BetaSeries/BetaSeries.swift
+++ BetaSeries-Mac/BetaSeries/BetaSeries.swift
@@ -54,7 +54,6 @@ struct BetaSeriesClient {
         
         _ = semaphore.wait(timeout: .distantFuture)
         let reply = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)! as String
-        print(reply)
         if let dataJson = convertStringToDictionary(json: reply) {
             return dataJson
         }
@@ -64,23 +63,44 @@ struct BetaSeriesClient {
     
     func GetListSeries( ) {
         print("GetListSeries")
-        var shows = [String]()
         
         let url = base_url + "episodes/list"
         let params:[String:String] = [
             "token" : token
         ]
+        var dict:[Show] = []
         
         if let ret = MakeRequestWith(url: url, requestMethod: "GET", params:params) {
-            let episodes = ret["shows"] as? [[String: Any]] ?? []
-            for episode in episodes {
-                if let title = episode["title"] as? String {
-                    shows.append(title)
+            let shows = ret["shows"] as? [[String: Any]] ?? []
+            for show in shows {
+                let id = show["id"] as! Int
+                let title = show["title"] as! String
+                var unseen:[Episode] = []
+                
+                let showsUnseen = show["unseen"] as? [[String: Any]] ?? []
+                for showsEpisode in showsUnseen {
+                    unseen.append(
+                        Episode(
+                            id: showsEpisode["id"] as! Int,
+                            title: showsEpisode["title"] as! String,
+                            saison: showsEpisode["season"] as! Int,
+                            nb: showsEpisode["episode"] as! Int,
+                            description: showsEpisode["description"] as! String,
+                            special: showsEpisode["special"] as! Int,
+                            link: showsEpisode["resource_url"] as! String,
+                            date: showsEpisode["date"] as! String
+                        )
+                    )
                 }
+                
+                dict.append( Show(id: id, title: title, episodesunseen: unseen) )
             }
         }
         
-        print(shows)
+        dict.forEach { (Show) in
+            print( Show.Title )
+        }
+        
     }
     
     func GetEpisodesOfSerie( ) {
