7a9f9d6a958be9ad62591f3352278f51d90a54e7
Victor Piolin <victor.piolin@gmail.com>
2017-11-27 15:26:15 +0100

    Add ViewController for ListShow

---
+++ BetaSeries-Mac/ListShowController.swift
@@ -0,0 +1,18 @@
+//
+//  ListShowController.swift
+//  BetaSeries-Mac
+//
+//  Created by Victor Piolin on 27/11/2017.
+//  Copyright © 2017 Victor Piolin. All rights reserved.
+//
+
+import Cocoa
+
+class ListShowController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
